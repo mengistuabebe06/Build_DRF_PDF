1f90754b8b4431c713c9dc8e743cf80b0f76c09c
mengistu abebe <ethiomengistu@gmail.com>
2024-04-12 18:26:41 +0300

    deploy my work

---
+++ backend/Build_DRF/mongodb_settings_template.py
@@ -0,0 +1,11 @@
+# mongodb_settings_template.py
+
+from django.conf import settings
+
+settings.DATABASES["default"]["ENGINE"] = "djongo"
+settings.DATABASES["default"]["NAME"] = "Cluster0"
+settings.DATABASES["default"]["CLIENT"] = {
+    "host": "__HOST__",
+    "username": "__USERNAME__",
+    "password": "__PASSWORD__",
+}
