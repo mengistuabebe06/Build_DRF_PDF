cdeed69fe588d06388ecf6ee751f2fb1f5e582ee
mengistu abebe <ethiomengistu@gmail.com>
2024-04-12 21:21:53 +0300

    build and deploy

--- backend/Build_DRF/mongodb_settings_template.py
+++ backend/Build_DRF/mongodb_settings_template.py
@@ -1,11 +1,18 @@
 import os
-
 from django.conf import settings
 
-settings.DATABASES["ENGINE"] = "djongo"
-settings.DATABASES["NAME"] = "Cluster0"
+# Access database information from settings.py
+db_engine = settings.DATABASES["default"]["ENGINE"]
+db_name = settings.DATABASES["default"]["NAME"]
+db_host = settings.DATABASES["default"]["CLIENT"]["host"]
+db_username = settings.DATABASES["default"]["CLIENT"]["username"]
+db_password = settings.DATABASES["default"]["CLIENT"]["password"]
+
+# Use the database information in your code
+settings.DATABASES["ENGINE"] = db_engine
+settings.DATABASES["NAME"] = db_name
 settings.DATABASES["CLIENT"] = {
-    "host": "__HOST__",
-    "username": "__USERNAME__",
-    "password": "__PASSWORD__",
+    "host": db_host,
+    "username": db_username,
+    "password": db_password,
 }
