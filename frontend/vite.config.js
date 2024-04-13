ea590a9d97ed2d9791b047376e005017a6a0555a
mengistu abebe <ethiomengistu@gmail.com>
2024-04-13 10:18:17 +0300

    Deploy for production

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
-  plugins: [react()],
-})
+   plugins: [react()],
+//  base:"/Build_DRF_PDF/"
+  build: {
+    outDir: 'dist', // Output directory for production build
+  },
+  
+ 
+
+});
+
