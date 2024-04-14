1c280b683e3df942e917fa18a70b0c4ff5350288
mengistu abebe <ethiomengistu@gmail.com>
2024-04-14 23:15:04 +0300

    Deploy for Production

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/Build_DRF_PDF/frontend/",
+  base:"/Build_DRF_PDF/",
   plugins: [react()],
 
 })
